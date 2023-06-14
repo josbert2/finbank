85329a38823d957d47f9c426e87fc7d163313c12
unknown <joheandroid@gmail.com>
2023-06-13 20:33:36 -0400

    migrate to prisma and mongodb

---
+++ pages/api/getCountAllBanks.js
@@ -0,0 +1,9 @@
+import Prisma from '@prisma/client'
+
+const { PrismaClient } = Prisma
+const prisma = new PrismaClient()
+
+export default async function handler(req, res) {
+
+    return res.status(200).json({ count: await prisma.account.count() })
+}
